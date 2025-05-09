d25b5e7cfcd68923054bcb0da24135642ae34e32
fabio henrique <henri8274@gmail.com>
2025-05-09 10:17:38 -0300

    updatee

--- server.js
+++ server.js
@@ -2,7 +2,8 @@ const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
+
 
 // Middleware
 app.use(cors());
